1b77279cdc97adfec51dda2aeaa1840c2db68215
leandro <leandro.agustin20@outlook.com>
2023-03-31 08:53:50 -0300

    validacion del jwt

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -5,23 +5,24 @@ import userUpdateDataDTO from "../dto/user-update.data.dto.js"
 import userUpdateEmailDTO from "../dto/user-update.email.dto.js"
 import userUpdatePasswordDTO from "../dto/user-update.password.dto.js"
 import userUnregisterDTO from "../dto/user-unregister.dto.js"
+import userJWTDTO from "../dto/user-jwt.dto.js"
 
 const userRouter = Router()
 
 
-userRouter.get("/profile")
+userRouter.get("/profile", userJWTDTO)
 
 userRouter.post("/register", userRegisterDTO, (req, res) => {res.send()})
 
 userRouter.post("/login", userLoginDTO)
 
-userRouter.patch("/update-data", userUpdateDataDTO)
+userRouter.patch("/update-data", userJWTDTO, userUpdateDataDTO, )
 
-userRouter.patch("/update-email", userUpdateEmailDTO)
+userRouter.patch("/update-email", userJWTDTO, userUpdateEmailDTO)
 
-userRouter.patch("/update-password", userUpdatePasswordDTO)
+userRouter.patch("/update-password", userJWTDTO, userUpdatePasswordDTO)
 
-userRouter.delete("/unregister", userUnregisterDTO)
+userRouter.delete("/unregister", userJWTDTO, userUnregisterDTO)
 
 
 
